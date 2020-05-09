3639c6d48accf818121bf20720b196fbe2d766d2
marvin evins <maevins@cps.edu>
2020-05-08 23:05:57 -0500

    added csv parse file

--- AttendanceAppKing/HomeController.swift
+++ AttendanceAppKing/HomeController.swift
@@ -13,6 +13,11 @@ class HomeController: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var collection: UICollectionView!
 
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    
+    var student = [Student]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,13 +34,22 @@ class HomeController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
     }
     
+    //for parsing csv file
+    
+    func parseStudentCSV(){
+        
+    }
+    
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
          return 30
      }
      
      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StudentCell", for: indexPath) as? StudentCell{
-                   
+            
+            let student = Student(first_name: "dave", last_name: "donald", id: "3", email: "geraldman", gender: "M", tardy: 5, absence: 9)
+            cell.configureCell(student:student)
                    return cell
                }else{
                    return UICollectionViewCell()
